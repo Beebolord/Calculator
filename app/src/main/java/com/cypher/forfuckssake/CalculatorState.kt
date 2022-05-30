93a58626e3ddc24c7b5754ed1b7d325cc5440ff5
Ismaelforest <ismaelforest@gmail.com>
2022-05-30 01:23:44 -0400

    Very basic Calculator app

---
+++ app/src/main/java/com/cypher/forfuckssake/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.cypher.forfuckssake
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
---
+++ app/src/main/java/com/cypher/forfuckssake/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.cypher.forfuckssake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cypher.forfuckssake.ui.theme.ForfuckssakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ForfuckssakeTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+    Calculator(
+        state = state,
+    onAction = viewModel::onAction,
+    buttonSpacing = buttonSpacing,
+    modifier = Modifier
+        .fillMaxSize()
+        .background(Color.DarkGray))
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ForfuckssakeTheme {
+        Greeting("Android")
+    }
+}
